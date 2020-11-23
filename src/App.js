09eb1c3b697c218b3f51a12887a1280875afc325
bartek9105 <bartek9105@gmail.com>
2020-11-23 22:45:23 +0100

    Feature: fetch countries data, create component for country

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import './App.css';
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import Country from './components/Country'
 
 function App() {
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      try {
+        const fetchedCountries = await axios.get('https://restcountries.eu/rest/v2/all')
+        setCountries(fetchedCountries.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchCountries()
+  }, [])
+
   return (
     <div className="App">
-      <h1>React app</h1>      
+      <h1>Countries</h1>
+      { countries.map(country => (
+        <div key={country.name}>
+          <Country country={country}/>          
+        </div>
+      )) }
     </div>
   );
 }
---
+++ src/components/Country.js
@@ -0,0 +1,9 @@
+const Country = ({ country }) => {
+    return (
+        <div>
+            <span>{country.name}</span>
+        </div>
+    )
+}
+
+export default Country

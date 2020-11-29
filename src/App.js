cc88ae41802cc48702519f5674e4f7d94fa26439
bartek9105 <bartek9105@gmail.com>
2020-11-29 14:57:39 +0100

    Feature: Add loading spinner

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import Country from './components/Country/Country'
 import Navbar from './components/Navbar/Navbar'
 import SearchBar from './components/SearchBar/SearchBar'
 import FilterSelect from './components/FilterSelect/FilterSelect'
+import Spinner from './components/Spinner/Spinner'
 
 function App() {
   const [countries, setCountries] = useState([])
   const [query, setQuery] = useState('')
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     const fetchCountries = async () => {
       try {
         const fetchedCountries = await axios.get('https://restcountries.eu/rest/v2/all')
+        setIsLoading(false)
         setCountries(fetchedCountries.data)
       } catch (error) {
         console.log(error)
@@ -27,6 +30,7 @@ function App() {
       setQuery(query)
       if (query) {
         const foundCountry = await axios.get(`https://restcountries.eu/rest/v2/name/${query}`)
+        setIsLoading(false)
         setCountries(foundCountry.data)
       }
     } catch (error) {
@@ -44,6 +48,7 @@ function App() {
         </section>
         { query ? <span>Results for "{query}"</span> : null }
         <section className="countries-container">
+          { isLoading ? <Spinner /> : null }
           { countries.map(country => (
             <div key={country.name}>
               <Country country={country}/>          
